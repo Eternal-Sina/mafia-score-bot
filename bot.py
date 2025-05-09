3fca1b4b6ac627c3e5c791f0468dc521fc48e1e5
Eternal-Sina <Eternal-Sina@users.noreply.github.com>
2025-05-10 02:26:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
